7b145f0a6b835911120dde2727708baf315f4ee7
qostua <qostua@gmail.com>
2022-10-22 14:16:29 +0300

    Написал функцию проверки валидности введенной даты

--- js/validation.js
+++ js/validation.js
@@ -1,5 +1,8 @@
 const PHONE_INPUT_REGEXP = '\\+7 \\(\\d{3}\\) \\d{3}-\\d{2}-\\d{2}';
 const DATE_INPUT_REGEXP = '(\\d{2}/){2}\\d{4}';
+const MIN_DAYS_BEFORE_DELIVERY = 1;
+const MAX_DAYS_BEFORE_DELIVERY = 7;
+const MS_IN_DAY = 86400000;
 
 const phoneInputs = document.querySelectorAll('[data-input-type="tel"]');
 const dateInputs = document.querySelectorAll('[data-input-type="date"]');
@@ -39,17 +42,39 @@ const setCardInputValidity = (cardFieldset, isValid = false) => {
 
   inputs.forEach((input) => input.setCustomValidity(customValidity));
 };
-const setInputFieldsetValidity = (input, isValid = false) => {
-  const validityClass = isValid ? 'input-wrapper--success' : 'input-wrapper--error';
+const showInputFieldsetValidity = (input) => {
+  const validityClass = input.validity.valid ? 'input-wrapper--success' : 'input-wrapper--error';
   const inputFieldset = input.closest('fieldset');
 
   inputFieldset.classList.remove('input-wrapper--error', 'input-wrapper--success');
   inputFieldset.classList.add(validityClass);
+};
+const isInputDeliveryDateCorrect = (inputDateString = '01/01/1000') => {
+  const [inputDay, inputMonth, inputYear] = inputDateString.split('/');
+  const now = new Date();
+  const deliveryDate = new Date();
+
+  deliveryDate.setTime(now.getTime());
+  deliveryDate.setFullYear(Number(inputYear));
+  deliveryDate.setMonth(Number(inputMonth) - 1);
+  deliveryDate.setDate(Number(inputDay));
+
+  const calcedDay = deliveryDate.getDate();
+  const calcedMonth = deliveryDate.getMonth();
+  const calcedYear = deliveryDate.getFullYear();
+
+  const isInputDayCorrect = (calcedDay === Number(inputDay)) && (calcedMonth + 1 === Number(inputMonth)) && (calcedYear === Number(inputYear));
+
+  const dateDiff = (deliveryDate - now) / MS_IN_DAY;
+  const isInputDayMatch = (dateDiff <= MAX_DAYS_BEFORE_DELIVERY) && (dateDiff >= MIN_DAYS_BEFORE_DELIVERY);
+
+  return isInputDayCorrect && isInputDayMatch;
 };
 
 export {
   isFormValidity,
   isCardNumberCorrect,
   setCardInputValidity,
-  setInputFieldsetValidity
+  showInputFieldsetValidity,
+  isInputDeliveryDateCorrect
 };
