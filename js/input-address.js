ec10bf7f85ba2e9e4f0c66161258b8c32e189061
qostua <qostua@gmail.com>
2022-10-24 15:36:48 +0300

    Создал шаблон модуля для генерации разметки из данных с сервера

---
+++ js/input-address.js
@@ -0,0 +1,25 @@
+const generateCityInputsList = (data) => {
+
+};
+const renderCityInputsLists = (cityInputs) => {
+
+};
+
+const generateAddressInputsList = (data) => {
+
+};
+const renderAddressInputsList = (addressInputs) => {
+
+};
+
+const setCityInput = (cb) => {
+
+};
+
+export {
+  renderCityInputsLists,
+  generateCityInputsList,
+  renderAddressInputsList,
+  generateAddressInputsList,
+  setCityInput
+};
