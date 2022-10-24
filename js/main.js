70f82cea944535de897539e73ee023ae60bc274a
qostua <qostua@gmail.com>
2022-10-24 20:53:32 +0300

    Подключил модуль для работы с картой в точку входа

--- js/main.js
+++ js/main.js
@@ -1,21 +1,48 @@
 import './keyboard-control.js';
 import './form.js';
 import {
+  getDeliveryPointsData,
   renderCityInputsLists,
   renderAddressInputsList,
-  setCityInput
+  setCityInput,
+  setAddressInput,
+  getCheckedAddressData
 } from './input-address.js';
 import {
   getData
 } from './api.js';
+import {
+  getMap,
+  renderMarkers,
+  activeMarker
+} from './map.js';
 
-getData(
-  (data) => {
-    renderCityInputsLists(data);
-    renderAddressInputsList(data);
-    setCityInput(
-      () => renderAddressInputsList(data)
-    );
-  }
-);
+const MapOptions = {
+  ANIMATE_MAP: true,
+  STATIC_MAP: false,
+}
+
+getMap(
+  () => getData(
+    (data) => {
+      renderCityInputsLists(data);
+      renderAddressInputsList(data);
+      renderMarkers(getDeliveryPointsData(data));
+      activeMarker(getCheckedAddressData(data), MapOptions.STATIC_MAP);
+      setCityInput(
+        () => {
+          renderAddressInputsList(data);
+          renderMarkers(getDeliveryPointsData(data));
+          activeMarker(getCheckedAddressData(data), MapOptions.STATIC_MAP);
+        }
+      );
+      setAddressInput(
+        () => {
+          activeMarker(getCheckedAddressData(data), MapOptions.ANIMATE_MAP);
+        }
+      )
+    }
+  )
+)
+
 
